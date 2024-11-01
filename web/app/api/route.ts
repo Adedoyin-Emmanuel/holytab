8b5efda75fe3785be996329cd9e1bac5e654f40b
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-01 17:48:00 +0100

    feat: updated base API route

--- web/app/api/route.ts
+++ web/app/api/route.ts
@@ -1,10 +1,10 @@
 import { NextResponse } from "next/server";
 
-export default async function GET(req: Request) {
+export const GET = async (req: Request) => {
   return NextResponse.json({
     message:
       "Hi welcome to the Holy Tabs API. This API powers HolyTab chrome extension and web app. Please do not abuse. Thank you and God bless you.",
     data: {},
     status: 200,
   });
-}
+};
