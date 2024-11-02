57c0c51667c66b431cd926433f01d3d880ac90f2
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-02 17:00:19 +0100

    feat: updated API

--- web/app/api/confession/route.ts
+++ web/app/api/confession/route.ts
@@ -1,5 +1,53 @@
 import { NextResponse } from "next/server";
 import Joi from "joi";
+import fs from "fs";
+import path from "path";
+import { confessionSchema } from "../schema/schema";
 
-export const POST = (req: Request) => {
+const dataPath = path.join(process.cwd() + "/data", "data.json");
+
+const readData = () => {
+  const data = fs.readFileSync(dataPath, "utf-8");
+  return JSON.parse(data);
+};
+
+export const POST = async (req: Request) => {
+  try {
+    const body = await req.json();
+    const { error, value } = confessionSchema.validate(body);
+    if (error) {
+      return NextResponse.json(
+        { error: error.details[0].message },
+        { status: 400 }
+      );
+    }
+
+    const { skip, take, search } = value;
+
+    const data = readData();
+    let confessions = data.affirmations;
+
+    if (search) {
+      confessions = confessions.filter((confession: string) =>
+        confession.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+
+    const paginatedConfessions = confessions.slice(skip, skip + take);
+
+    return NextResponse.json({
+      message: "Confessions retrived successfully",
+      success: true,
+      data: {
+        total: confessions.length,
+        count: paginatedConfessions.length,
+        confessions: paginatedConfessions,
+      },
+    });
+  } catch (err) {
+    return NextResponse.json(
+      { message: "Failed to process request", data: {}, success: false },
+      { status: 500 }
+    );
+  }
 };
