5cff15e1b75c81c7bb04795058016f71a0df04d6
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-03 22:12:49 +0100

    feat: added block duration

--- web/app/api/constants/index.ts
+++ web/app/api/constants/index.ts
@@ -1,2 +1,3 @@
 export const MAX_REQUEST_PER_MINUTE = 10;
 export const MAX_DURATION = 60;
+export const BLOCK_DURATION = 60;
--- web/app/api/utils/rateLimiter.ts
+++ web/app/api/utils/rateLimiter.ts
@@ -1,7 +1,12 @@
 import { RateLimiterMemory } from "rate-limiter-flexible";
-import { MAX_DURATION, MAX_REQUEST_PER_MINUTE } from "./../constants";
+import {
+  MAX_DURATION,
+  MAX_REQUEST_PER_MINUTE,
+  BLOCK_DURATION,
+} from "./../constants";
 
 export const rateLimiter = new RateLimiterMemory({
   points: MAX_REQUEST_PER_MINUTE,
   duration: MAX_DURATION,
+  blockDuration: BLOCK_DURATION,
 });
