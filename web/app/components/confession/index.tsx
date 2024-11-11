be477bc4d0df6fd2ec4087a499b5c01b628dce9e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-11 22:10:40 +0100

    feat: updated confession component

--- web/app/components/confession/index.tsx
+++ web/app/components/confession/index.tsx
@@ -1,19 +1,21 @@
+import localFont from "next/font/local";
+import { cn } from "@/lib/utils";
 
-"use client"
-import React from "react";
+const timesNewRoman = localFont({
+  src: "./../public/fonts/timesNewRoman.ttf",
+});
 
-interface ConfessionProps {
-    
-}
-
-const Confession = ()=> {
-
-    return (
-        <div>
-            <h1>Confession works!</h1>
-        </div>
-    );  
-}
+const Confession = () => {
+  return (
+    <h1
+      className={cn(
+        timesNewRoman.className,
+        "font-normal md:text-[3.25rem] text-2xl leading-[3.74rem] tracking-[-0.02em] p-5 md:w-8/12 text-center md:leading-relaxed"
+      )}
+    >
+      I am the LORD, the God of all mankind. Is anything too hard for me?
+    </h1>
+  );
+};
 
 export default Confession;
-    
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import localFont from "next/font/local";
-import { cn } from "@/lib/utils";
 
 import { Axios } from "@/config/axios";
 import { Confession } from "@/interfaces";
@@ -10,9 +8,6 @@ import Search from "@/app/components/search";
 import SocialIcons from "@/app/components/social-icons";
 import ConfessionBadge from "@/app/components/confession-badge";
 
-const timesNewRoman = localFont({
-  src: "./../public/fonts/timesNewRoman.ttf",
-});
 
 export default function Home() {
   const [confessionData, setConfessionData] = useState<Confession>();
@@ -43,14 +38,7 @@ export default function Home() {
 
       <section className="w-full flex items-center flex-col">
         <ConfessionBadge />
-        <h1
-          className={cn(
-            timesNewRoman.className,
-            "font-normal md:text-[3.25rem] text-2xl leading-[3.74rem] tracking-[-0.02em] p-5 md:w-8/12 text-center md:leading-relaxed"
-          )}
-        >
-          I am the LORD, the God of all mankind. Is anything too hard for me?
-        </h1>
+      
       </section>
 
       <br />
