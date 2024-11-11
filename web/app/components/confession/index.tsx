76ca00e5e514fe194abbcb8a7db68fcb0381005a
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-11 22:09:49 +0100

    feat: added confession component

---
+++ web/app/components/confession/index.tsx
@@ -0,0 +1,19 @@
+
+"use client"
+import React from "react";
+
+interface ConfessionProps {
+    
+}
+
+const Confession = ()=> {
+
+    return (
+        <div>
+            <h1>Confession works!</h1>
+        </div>
+    );  
+}
+
+export default Confession;
+    
