912cabecfc81e4921aca081da4a11e3fe661639f
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-11 22:05:16 +0100

    feat: updaeted social icons component

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,9 +2,7 @@
 import React, { useEffect, useState } from "react";
 import localFont from "next/font/local";
 import { cn } from "@/lib/utils";
-import { IoLogoWhatsapp, IoLogoTwitter } from "react-icons/io";
-import { SiTelegram, SiReddit } from "react-icons/si";
-import { FaFacebook } from "react-icons/fa";
+
 
 import { Axios } from "@/config/axios";
 import { Confession } from "@/interfaces";
@@ -15,13 +13,7 @@ const timesNewRoman = localFont({
 });
 
 export default function Home() {
-  const socialIcons = [
-    { id: "whatsapp", icon: <IoLogoWhatsapp /> },
-    { id: "facebook", icon: <FaFacebook /> },
-    { id: "twitter", icon: <IoLogoTwitter /> },
-    { id: "telegram", icon: <SiTelegram /> },
-    { id: "reddit", icon: <SiReddit /> },
-  ];
+
 
   const [confessionData, setConfessionData] = useState<Confession>();
 
@@ -73,18 +65,7 @@ export default function Home() {
       <br />
 
       <p className="uppercase text-sm">Share on your socials below</p>
-      <section className="flex items-center justify-center gap-10 flex-wrap">
-        {socialIcons.map(({ id, icon }) => (
-          <section
-            key={id}
-            className="md:w-16 md:h-16 w-10 h-10 border-[1px] border-black bg-[#D9D9D9] dark:border-[#F5B700] dark:bg-black rounded-full flex items-center justify-center cursor-pointer hover:scale-110 transition-transform ease-in-out duration-150"
-          >
-            {React.cloneElement(icon, {
-              className: "md:w-5 md:h-5 w-4 h-4 dark:text-[#F5B700]",
-            })}
-          </section>
-        ))}
-      </section>
+     
     </div>
   );
 }
