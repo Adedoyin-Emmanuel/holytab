40bbb833bae4c59c9e2c4be131cfabf4e6a95f43
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-11 10:27:09 +0100

    feat: updated metadata

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -7,12 +7,38 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 
 const bai_jamjuree = Bai_Jamjuree({ subsets: ["latin"], weight: "400" });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Holy Tab",
   description:
-    "Fresh, Bible-centered confession on every new tab. Stay inspired and rooted in faith throughout your day",
+    "Get fresh, Bible-centered confessions on every new tab. Stay inspired and rooted in faith throughout your day.",
+  keywords:
+    "Bible, faith, confessions, daily inspiration, Holy Tab, scripture, new tab motivation",
+  author: "Emmysoft X Frames 56",
+  openGraph: {
+    title: "Holy Tab - Bible-Centered Confessions",
+    description:
+      "Stay inspired and rooted in faith with Bible-centered confessions on every new tab.",
+    type: "website",
+    url: "https://holytab.adedoyin.dev",
+    images: [
+      {
+        url: "https://holytab.adedoyin.dev/holy.png",
+        width: 1080,
+        height: 1080,
+        alt: "Holy Tab - Bible-Centered Confessions",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@frames_arc",
+    creator: "@Emmysoft_Tm",
+    title: "Holy Tab - Bible-Centered Confessions",
+    description:
+      "Fresh, Bible-centered confession on every new tab. Stay inspired and rooted in faith throughout your day.",
+    image: "https://holytab.adedoyin.dev/holy.png",
+  },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
