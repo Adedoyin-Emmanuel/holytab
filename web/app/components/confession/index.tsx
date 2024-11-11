dd01f2b920a945d656548b5e64000fd0bba9ddef
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-11 22:34:45 +0100

    feat: updated the confession component

--- web/app/components/confession/index.tsx
+++ web/app/components/confession/index.tsx
@@ -1,21 +1,39 @@
 import localFont from "next/font/local";
 import { cn } from "@/lib/utils";
+import { Axios } from "@/config/axios";
 
 const timesNewRoman = localFont({
   src: "./../../../public/fonts/timesNewRoman.ttf",
 });
 
-const Confession = () => {
-  return (
-    <h1
-      className={cn(
-        timesNewRoman.className,
-        "font-normal md:text-[3.25rem] text-2xl leading-[3.74rem] tracking-[-0.02em] p-5 md:w-8/12 text-center md:leading-relaxed"
-      )}
-    >
-      I am the LORD, the God of all mankind. Is anything too hard for me?
-    </h1>
-  );
+const Confession = async () => {
+  const response = await Axios.post("/confession", {
+    take: 1,
+  });
+
+  if (response.status === 200) {
+    return (
+      <h1
+        className={cn(
+          timesNewRoman.className,
+          "font-normal md:text-[3.25rem] text-2xl leading-[3.74rem] tracking-[-0.02em] p-5 md:w-8/12 text-center md:leading-relaxed"
+        )}
+      >
+        {response.data.data.confessions[0]}
+      </h1>
+    );
+  } else {
+    return (
+      <h1
+        className={cn(
+          timesNewRoman.className,
+          "font-normal md:text-[3.25rem] text-2xl leading-[3.74rem] tracking-[-0.02em] p-5 md:w-8/12 text-center md:leading-relaxed"
+        )}
+      >
+        I am the LORD, the God of all mankind. Is anything too hard for me?
+      </h1>
+    );
+  }
 };
 
 export default Confession;
