15867141330fe4603760f9fdf8747b13e61f5cc5
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-12 15:09:06 +0100

    feat: updated confession component

--- extension/src/components/confession/index.tsx
+++ extension/src/components/confession/index.tsx
@@ -1,18 +1,16 @@
+import { cn } from "@/lib/utils";
 
-import React from "react";
-
-interface ConfessionProps {
-    
-}
-
-const Confession = () => {
-
-    return (
-        <div>
-            <h1>Confession works!</h1>
-        </div>
-    );  
-}
+const Confession = ({ text }: { text: string }) => {
+  return (
+    <h1
+      className={cn(
+        "timesNewRoman",
+        "font-normal md:text-[3.25rem] text-2xl leading-[3.74rem] tracking-[-0.02em] p-5 md:w-8/12 text-center md:leading-relaxed"
+      )}
+    >
+      {text}
+    </h1>
+  );
+};
 
 export default Confession;
-    
