66371155624a0014700285d87bf27aa424ff0dc9
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-12 14:59:44 +0100

    feat: updated confession badge component

--- extension/src/components/confession-badge/index.tsx
+++ extension/src/components/confession-badge/index.tsx
@@ -1,18 +1,10 @@
-
-import React from "react";
-
-interface ConfessionBadgeProps {
-    
-}
-
 const ConfessionBadge = () => {
-
-    return (
-        <div>
-            <h1>ConfessionBadge works!</h1>
-        </div>
-    );  
-}
+  return (
+    <section className="flex items-center gap-[6px] bg-[#F28C8C33] dark:bg-[#F5B7001A] rounded-[10px] pt-2 pr-3 pb-2 pl-3">
+      <section className="bg-[#FA7272] dark:bg-[#F5B700] h-[14px] w-[14px] rounded-full "></section>
+      <p className="text-sm ">CONFESSION OF THE DAY</p>
+    </section>
+  );
+};
 
 export default ConfessionBadge;
-    
