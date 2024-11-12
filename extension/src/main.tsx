fd664dd19048d956589dbcc2fb4d103345a55dc7
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-12 14:55:37 +0100

    feat: added theme provider to extension layout

--- extension/src/main.tsx
+++ extension/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-createRoot(document.getElementById('root')!).render(
+import "./index.css";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <App />
+    </NextThemesProvider>
+  </StrictMode>
+);
