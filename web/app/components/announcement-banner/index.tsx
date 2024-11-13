5095bc807c6f4e55acf23b9489307d85d18a0e1f
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-13 03:59:18 +0100

    feat: added announcement banner

---
+++ web/app/components/announcement-banner/index.tsx
@@ -0,0 +1,19 @@
+
+"use client"
+import React from "react";
+
+interface AnnouncementBannerProps {
+    
+}
+
+const AnnouncementBanner = ()=> {
+
+    return (
+        <div>
+            <h1>AnnouncementBanner works!</h1>
+        </div>
+    );  
+}
+
+export default AnnouncementBanner;
+    
