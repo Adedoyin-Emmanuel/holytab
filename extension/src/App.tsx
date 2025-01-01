c83c8782515b8c844b0b863a831e531efe92d658
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-01-01 03:32:19 +0100

    chore: updated light mode color for chrome extension

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -31,7 +31,7 @@ const App = () => {
   }, []);
 
   return (
-    <div className="w-screen h-screen flex items-center justify-center gap-5 flex-col dark:dark-bg bg-[#EBEBEB]">
+    <div className="w-screen h-screen flex items-center justify-center gap-5 flex-col">
       <SettingsMenu />
 
       <Search />
